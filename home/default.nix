aa5859c87d663f948700c15db5be6ef41dbdf377
= <=>
2026-05-19 19:27:36 +0200

    forgot ;

--- home/default.nix
+++ home/default.nix
@@ -32,7 +32,7 @@
     enable = true;
     settings = {
       user.name = "nimjit";
-      user.email = "tidemanus@gmail.com"
+      user.email = "tidemanus@gmail.com";
       init.defaultBranch = "main";
       pull.rebase = false;
     };
