dfd8e9031a976627c2a20ee50e3a09032b4b6818
Thijmen Nouwens <t.j.nouwens@student.tudelft.nl>
2026-05-19 15:35:27 +0200

    changed name of wallpaper for syntax issue

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -17,7 +17,7 @@
     # Stylix sets the wallpaper system-wide.
     # Add an image to themes/wallpapers/ and set the path here.
     # Until you add one, comment this out and set image above instead.
-    image = ../themes/wallpapers/Pieter_Bruegel_the_Elder_-_Children's_Games_-_Google_Art_Project.jpg;
+    image = ../themes/wallpapers/Pieter_Bruegel-Childrens_Games.jpg;
 
     # ── Fonts ─────────────────────────────────────────────────────────────
     fonts = {
