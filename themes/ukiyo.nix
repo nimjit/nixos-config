6a01aaafeb8c0d7d5a352f8a77fc635eea2b1251
nimjit <tidemanus@gmail.com>
2026-05-21 09:06:59 +0200

    added comments to theme

--- themes/ukiyo.nix
+++ themes/ukiyo.nix
@@ -1,18 +1,18 @@
 scheme: "Ukiyo"
 author: "Thijmen"
-base00: "372d29"
-base01: "2b2723"
-base02: "413632"
-base03: "504431"
-base04: "645743"
-base05: "ccc2b7"
-base06: "b2a699"
-base07: "868074"
-base08: "c72626"
-base09: "e77a59"
-base0A: "ba945f"
-base0B: "281f1b"
-base0C: "2e2420"
-base0D: "e0ba86"
-base0E: "615450"
-base0F: "514541"
+base00: "372d29"  # Main      Background
+base01: "2b2723"  # Lighter   Background
+base02: "413632"  # Selection Background
+base03: "504431"  # Comments / Darkest gray
+base04: "645743"  # Secondary Text
+base05: "ccc2b7"  # Main      Text
+base06: "b2a699"  # 'lighter' Text
+base07: "868074"  # 'lightest'Text
+base08: "c72626"  # Red
+base09: "e77a59"  # Orange
+base0A: "ba945f"  # Yellow
+base0B: "281f1b"  # Green
+base0C: "2e2420"  # Cyan
+base0D: "e0ba86"  # Blue
+base0E: "615450"  # Magenta            
+base0F: "514541"  # Brown
