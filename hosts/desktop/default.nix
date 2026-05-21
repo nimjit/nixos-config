645636cc54707d0fd30c55929c39bfc0875cf4fd
nimjit <tidemanus@gmail.com>
2026-05-21 08:57:43 +0200

    added nvidia hardware management (supposed to be good for my 1060)

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -23,7 +23,7 @@
   # Nvidia (proprietary):
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia.modesetting.enable = true;
-  hardware.nvidia.powerManagement.enable = false;
+  hardware.nvidia.powerManagement.enable = true;
   hardware.nvidia.open = false;
   hardware.nvidia.nvidiaSettings = true;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_535;
