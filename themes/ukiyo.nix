c907f3b1a9434ca762d88f383d5674068200db02
nimjit <tidemanus@gmail.com>
2026-05-21 14:12:53 +0200

    changed ukiyo theme colours

--- themes/ukiyo.nix
+++ themes/ukiyo.nix
@@ -1,18 +1,18 @@
 scheme: "Ukiyo"
 author: "Thijmen"
 base00: "372d29"  # Main      Background
-base01: "2b2723"  # Lighter   Background
-base02: "413632"  # Selection Background
-base03: "504431"  # Comments / Darkest gray
-base04: "645743"  # Secondary Text
+base01: "413632"  # Lighter   Background
+base02: "504431"  # Selection Background
+base03: "868074"  # Comments / Darkest gray
+base04: "b2a699"  # Secondary Text
 base05: "ccc2b7"  # Main      Text
-base06: "b2a699"  # 'lighter' Text
-base07: "868074"  # 'lightest'Text
+base06: "ddd5cc"  # 'lighter' Text
+base07: "eee8e2"  # 'lightest'Text
 base08: "c72626"  # Red
 base09: "e77a59"  # Orange
 base0A: "ba945f"  # Yellow
-base0B: "281f1b"  # Green
-base0C: "2e2420"  # Cyan
+base0B: "9aad6e"  # Green
+base0C: "7ab5a0"  # Cyan
 base0D: "e0ba86"  # Blue
-base0E: "615450"  # Magenta            
-base0F: "514541"  # Brown
+base0E: "ba945f"  # Magenta            
+base0F: "645743"  # Brown
